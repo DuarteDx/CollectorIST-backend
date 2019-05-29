54441009df611ee17d749b792e39a5235babde51
Duarte Dias <DuartexDias@gmail.com>
2019-05-29 13:10:04 +0100

    more cleanup

--- src/services/units/IsManagerInUnitService.js
+++
@@ -1,7 +0,0 @@
-import { Mongo } from 'porg'
-
-export default async ({ id, username }) => {
-  let db = await Mongo.getDB()
-  let unit = await db.collection('units').findOne({ '_id': id, managers: username })
-  return !!unit
-}
