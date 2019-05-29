9d5376174356f04e06521d121eff4032993a7004
Duarte Dias <DuartexDias@gmail.com>
2019-05-29 13:07:40 +0100

    major cleanup

--- src/services/oaipmh-export/ListRecordsService.js
+++
@@ -1,9 +0,0 @@
-import GetOAIPMHRootNodeService from './GetOAIPMHRootNodeService'
-
-const VERB = 'ListRecords'
-
-export default ({ path, resumptionToken }) => {
-  const rootNode = GetOAIPMHRootNodeService({ requestAttributes: { verb: VERB }, path })
-
-  return rootNode.end()
-}
